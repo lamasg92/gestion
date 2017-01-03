104a5f96f1a587e52f7417cc6afe067364fb9f65
valentin plechuc <vplechuc@gmail.com>
2017-01-03 13:11:44 -0300

    change Config app name, changed resources for loadiing css and js files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{--<link href="/css/app.css" rel="stylesheet">--}}
 
     <!-- Scripts -->
     <script>
@@ -82,6 +84,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="/js/app.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    {{--<script src="/js/app.js"></script>--}}
 </body>
 </html>
