e2273b4a97fc72d8bf3d3b5831ec9a97ac9ccae5
valentin plechuc <vplechuc@gmail.com>
2017-01-03 19:16:47 -0300

    refactor test to use a test database. (gestion_test)
    define a new connection to testdatabase  on database.php
    refator example to use a test database and transaction trait

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
@@ -11,9 +9,19 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
+    //using this trait insted of databasemigratoion to make tests faster
+    use DatabaseTransactions;
+
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Gestion');
+        $user = factory(App\User::class)->create([
+            'name' => 'Valentin Plechuc',
+        ]);
+
+        $this->actingAs($user, 'api');
+
+        $this->visit('/api/user')
+            ->see('Valentin Plechuc');
+
     }
 }
