92042e07d4d2b235d4733d426ccc856e270804d2
valentin plechuc <vplechuc@gmail.com>
2017-01-15 18:20:49 -0300

    adding integrationtest to category

--- tests/Integrations/CategoryIntegrationTest.php
+++ tests/Integrations/CategoryIntegrationTest.php
@@ -7,19 +7,24 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class CategoryIntegrationTest extends TestCase
 {
     use DatabaseTransactions;
+
+
     function test_a_category_is_saved_to_the_database()
     {
         $user = $this->defaultUser();
 
-        $category = factory(Category::class)->make([
-            'nombre' => 'Categoria1',
+        $category = factory(Category::class)->create([
+            'nombre' => 'categoria1',
+            'descripcion' => 'descripcion'
         ]);
 
 
-        $user->save($category);
+       // $user->save($category);
+
         $this->assertSame(
             'categoria1',
             $category->fresh()->nombre
         );
+
     }
 }
