ea681c346f3e47ad2a03d4057f12076e3c9085cb
valentin plechuc <vplechuc@gmail.com>
2017-01-22 20:23:24 -0300

    WIP: add register account test

---
+++ tests/features/RegistrationTest.php
@@ -0,0 +1,19 @@
+<?php
+
+class RegistrationTest extends TestCase
+{
+    public function test_a_user_can_create_an_account()
+    {
+        $this->visitRoute('register')
+            ->type('user2@user.com', 'email')
+            ->type('user2', 'username')
+            ->type('user', 'name')
+            ->press('Registrar');
+
+        $this->seeInDatabase('users', [
+            'email' => 'user2@user.com',
+            'username' => 'user2',
+            'name' => 'user',
+        ]);
+    }
+}
