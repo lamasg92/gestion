46270cd59edd6d65516db7844590ca4a9c8eac0e
valentin plechuc <vplechuc@gmail.com>
2017-01-27 15:49:50 -0300

    adding dialy report

---
+++ resources/views/report/sales/daily.blade.php
@@ -0,0 +1,18 @@
+@extends('app')
+
+@section('content')
+
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Categorias</div>
+                    <div class="panel-body">
+
+                            Reporte diario
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+@endsection
