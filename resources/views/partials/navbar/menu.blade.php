60998cdc24eacdda18a2cc65c5eded77c84d3eef
valentin plechuc <vplechuc@gmail.com>
2017-01-31 00:47:10 -0300

    Adding usuario menu - should display roles and allow to change User´s role. and User list

--- resources/views/partials/navbar/menu.blade.php
+++ resources/views/partials/navbar/menu.blade.php
@@ -2,12 +2,57 @@
     <!-- Left Side Of Navbar -->
     <ul class="nav navbar-nav">
         @if (!Auth::guest())
-            <li><a href="#">Facturas</a></li>
+            {{--<li><a href="#">Facturas</a></li>--}}
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                    Facturas <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu" role="menu">
+                    <li>
+                        <a href="{{ route('invoice.create') }}" >
+                            Nueva
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#"
+                           onclick="event.preventDefault();
+                                                         document.getElementById('invoice-search').submit();">
+                            Buscar
+                        </a>
+
+                    </li>
+                </ul>
+            </li>
+
+
             <li><a href="{{ route('clients.index') }}">Clientes</a></li>
+
             @if(Auth::user()->isAdmin())
                 <li><a href="#">Articulos</a></li>
                 <li><a href="{{ route('categories.index') }}">Categorias</a></li>
-                <li><a href="{{ route('roles.index') }}">Roles</a></li>
+
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                        Usuarios <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li>
+                            <a href="{{ route('roles.index') }}">
+                                Roles
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Cambio Rol
+                            </a>
+
+                        </li>
+                    </ul>
+                </li>
+
+
+
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                         Reportes <span class="caret"></span>
