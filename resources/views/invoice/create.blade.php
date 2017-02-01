1cb7bed4ed4f3f78d0b40d9387a4d2f83675a49b
valentin plechuc <vplechuc@gmail.com>
2017-02-01 16:03:47 -0300

    Adding invoice view

--- resources/views/invoice/create.blade.php
+++ resources/views/invoice/create.blade.php
@@ -5,31 +5,31 @@
     <div class="panel-body">
 
 
-        {{--{!! Form::open(['route' => 'articles.index', 'method'=>'GET', 'class'=>'navbar-form navbar-right pull-left', 'role' => 'search']) !!}--}}
-            {{--<div class="form-group">--}}
-                {{--{!! Form::text('nombre', null, ['class'=>'form-control', 'placeholder'=>'Nombre Articulo']) !!}--}}
-            {{--</div>--}}
-            {{--<button type="submit" class="btn btn-default">Buscar</button>--}}
-        {{--{!! Form::close() !!}--}}
+        {!! Form::open(['route' => 'articles.index', 'method'=>'GET', 'class'=>'navbar-form navbar-right pull-left', 'role' => 'search']) !!}
+            <div class="form-group">
+                {!! Form::text('nombre', null, ['class'=>'form-control', 'placeholder'=>'Nombre Articulo']) !!}
+            </div>
+            <button type="submit" class="btn btn-default">Buscar</button>
+        {!! Form::close() !!}
 
 
-        {{--{!! Form::open(['route' => 'categories.index', 'method'=>'GET', 'class'=>'navbar-form navbar-left pull-right', 'role' => 'search']) !!}--}}
-            {{--<div class="form-group">--}}
-                {{--{!! Form::text('nombre', null, ['class'=>'form-control', 'placeholder'=>'Nombre Ctegoria']) !!}--}}
-            {{--</div>--}}
-            {{--<button type="submit" class="btn btn-default">Buscar</button>--}}
-        {{--{!! Form::close() !!}--}}
+        {!! Form::open(['route' => 'categories.index', 'method'=>'GET', 'class'=>'navbar-form navbar-left pull-right', 'role' => 'search']) !!}
+            <div class="form-group">
+                {!! Form::text('nombre', null, ['class'=>'form-control', 'placeholder'=>'Nombre Ctegoria']) !!}
+            </div>
+            <button type="submit" class="btn btn-default">Buscar</button>
+        {!! Form::close() !!}
 
-        {{--{!! Form::open(['method' => 'POST', 'route' => 'invoice.store']) !!}--}}
+        {!! Form::open(['method' => 'POST', 'route' => 'invoice.store']) !!}
 
 
-        {{--<div class="form-group">--}}
-            {{--<div class="col-md-6 col-md-offset-4">--}}
-                {{--<button type="submit" class="btn btn-primary">Grabar</button>--}}
-            {{--</div>--}}
-        {{--</div>--}}
+        <div class="form-group">
+            <div class="col-md-6 col-md-offset-4">
+                <button type="submit" class="btn btn-primary">Grabar</button>
+            </div>
+        </div>
 
-        {{--{!! Form::close() !!}--}}
+        {!! Form::close() !!}
 
     </div>
 
