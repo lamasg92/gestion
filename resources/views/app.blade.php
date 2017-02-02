ebc0668ea4349d82f105c8098e8c1a5fee504a27
valentin plechuc <vplechuc@gmail.com>
2017-02-02 11:40:05 -0300

    -adding seeder to databse seeder -changing app view for all panels -remove unused code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -34,7 +34,7 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-md-12 ">
                 <div class="panel panel-default">
 
                     @include('flash::message')
--- resources/views/partials/navbar/header.blade.php
+++ resources/views/partials/navbar/header.blade.php
@@ -1,14 +1,5 @@
     <div class="navbar-header">
 
-        <!-- Collapsed Hamburger -->
-    {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">--}}
-    {{--<span class="sr-only">Toggle Navigation</span>--}}
-    {{--<span class="icon-bar"></span>--}}
-    {{--<span class="icon-bar"></span>--}}
-    {{--<span class="icon-bar"></span>--}}
-    {{--</button>--}}
-
-    <!-- Branding Image -->
         <a class="navbar-brand" href="{{ route('home') }}">
             {{ config('app.short_name', 'Gestion') }}
         </a>
