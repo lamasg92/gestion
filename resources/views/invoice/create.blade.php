34c77bca990293c168f4b5b045d1d98592003b7e
valentin plechuc <vplechuc@gmail.com>
2017-02-04 01:16:04 -0300

    refactoring views -adding sarching input for user and article autocomplete

--- resources/views/invoice/create.blade.php
+++ resources/views/invoice/create.blade.php
@@ -1,27 +1,40 @@
 @extends('app')
 
+@section('styles')
+    <link href="{{ asset('css/jquery.easy-autocomplete.css') }}" rel="stylesheet">
+@endsection
+
+
 @section('content')
     <div class="panel-heading">Facturacion</div>
     <div class="panel-body">
 
-
-        {!! Form::open(['route' => 'articles.index', 'method'=>'GET', 'class'=>'navbar-form navbar-right pull-left', 'role' => 'search']) !!}
-            <div class="form-group">
-                {!! Form::text('nombre', null, ['class'=>'form-control', 'placeholder'=>'Nombre Articulo']) !!}
-            </div>
-            <button type="submit" class="btn btn-default">Buscar</button>
-        {!! Form::close() !!}
+        {!! Field::text('fecha', null ,['class' => 'form-control']) !!}
+        {!! Field::text('numero', null ,['class' => 'form-control']) !!}
 
 
-        {!! Form::open(['route' => 'clients.index', 'method'=>'GET', 'class'=>'navbar-form navbar-left pull-right', 'role' => 'search']) !!}
-            <div class="form-group">
-                {!! Form::text('nombre', null, ['class'=>'form-control', 'placeholder'=>'Nombre Cliente']) !!}
-            </div>
-            <button type="submit" class="btn btn-default">Buscar</button>
-        {!! Form::close() !!}
+        <div class="form-group">
+            {!! Form::open([ 'class'=>'form']) !!}
+               {!! Field::text('article',  ['class'=>'easy-autocomplete']) !!}
+            {!! Form::close() !!}
 
-        {!! Form::open(['method' => 'POST', 'route' => 'invoice.store']) !!}
+            {!! Form::open([ 'class'=>'form']) !!}
+                {!! Field::text('user',  ['class'=>'easy-autocomplete']) !!}
+                {!! Field::hidden('user_id', null, ['id' => 'user_id']) !!}
+            {!! Form::close() !!}
 
+        </div>
+    {!! Form::open(['method' => 'POST', 'route' => 'invoices.store']) !!}
+
+        <div class="form-group">
+
+
+
+        </div>
+        <div class="form-group">
+            {!! Form::label('payment_id', 'Forma de Pago') !!}
+            {!! Form::select('payment_id', $payments, null, ['class' => 'form-control', 'placeholder' => 'Seleccion una opcion', 'required'])!!}
+        </div>
 
         <div class="form-group">
             <div class="col-md-6 col-md-offset-4">
@@ -29,9 +42,100 @@
             </div>
         </div>
 
-        {!! Form::close() !!}
+    {!! Form::close() !!}
 
     </div>
 
+
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/jquery.easy-autocomplete.js') }}"></script>
+
+    <script>
+        $(document).ready(function () {
+            var user_options = {
+                url: "/invoices/users",
+                getValue: "name",
+
+                template: {
+                    type: "description",
+                    fields: {
+                        description: "username"
+                    }
+                },
+                list: {
+                    match: {
+                        enabled: true
+                    },
+                    onSelectItemEvent: function() {
+                        var user = $("#user").getSelectedItemData();
+                        $('#user_id').val(user.id);
+                    },
+                    onClickEvent: function () {
+                        var user = $("#user").getSelectedItemData();
+                        window.location.href = '/users/' + user.id;
+                    }
+                },
+                theme: "bootstrap",
+
+                ajaxSettings: {
+                    dataType: "json",
+                    method: "GET",
+                    data: {
+                    }
+                },
+
+                preparePostData: function(data) {
+                    data.term = $("#user").val();
+                    return data;
+                },
+
+                requestDelay: 400
+            };
+
+            $("#user").easyAutocomplete(user_options);
+
+        });
+    </script>
+    <script>
+        $(document).ready(function () {
+            var article_options = {
+                url: "/invoices/articles",
+                getValue: "nombre",
+
+                template: {
+                    type: "description",
+                    fields: {
+                        description: "descripcion"
+                    }
+                },
+                list: {
+                    match: {
+                        enabled: true
+                    }
+                },
+
+                theme: "bootstrap",
+
+                ajaxSettings: {
+                    dataType: "json",
+                    method: "GET",
+                    data: {
+                    }
+                },
+
+                preparePostData: function(data) {
+                    data.term = $("#user").val();
+                    return data;
+                },
+
+                requestDelay: 400
+            };
+
+            $("#article").easyAutocomplete(article_options);
+
+        });
+    </script>
 
 @endsection
--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -1,29 +1,14 @@
 @extends('app')
 
-@section('styles')
-    <link href="{{ asset('css/jquery.easy-autocomplete.css') }}" rel="stylesheet">
-@endsection
-
 @section('content')
 
     <div id=panel-tittle class="panel-heading">Facturas</div>
     <div class="panel-body">
 
-        <a href="{{route('invoice.create')}}" class="btn btn-info">
+        <a href="{{route('invoices.create')}}" class="btn btn-info">
             <i class="glyphicon glyphicon-new-window"></i>
             Nueva
         </a>
-
-        {!! Form::open([ 'class'=>'form']) !!}
-            {!! Field::text('country',  ['class'=>'easy-autocomplete']) !!}
-        {!! Form::close() !!}
-
-        {{--{!! Form::open(['route' => 'invoice.index', 'method'=>'GET', 'class'=>'navbar-form navbar-left pull-right', 'role' => 'search']) !!}--}}
-            {{--<div class="form-group">--}}
-                {{--{!! Form::text('numero', null, ['class'=>'form-control', 'placeholder'=>'Numero Factura']) !!}--}}
-            {{--</div>--}}
-              {{--<button type="submit" class="btn btn-default">Buscar</button>--}}
-        {{--{!! Form::close() !!}--}}
 
         <table class="table table-striped">
             <thead>
@@ -42,28 +27,3 @@
     </div>
 @endsection
 
-@section('scripts')
-    <script src="{{ asset('js/jquery.easy-autocomplete.js') }}"></script>
-
-    <script>
-        $(document).ready(function () {
-            var options = {
-
-                url: "/resources/country.json",
-
-                getValue: "name",
-
-                list: {
-                    match: {
-                        enabled: true
-                    }
-                },
-
-                theme: "bootstrap"
-            };
-
-            $("#country").easyAutocomplete(options);
-
-        });
-    </script>
-@endsection
