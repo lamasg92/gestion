79029db37ed6bfbdb09cb4e0f109276d39cad946
valentin plechuc <vplechuc@gmail.com>
2017-02-04 01:14:34 -0300

    -adding more users when regenerate the DB

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -21,11 +21,34 @@ class UsersTableSeeder extends Seeder
             'password' => bcrypt('admin'),
             // 'role_id' => Role::where('nombre','admin')->first()->id,
         ]);
-
-
         $user->role_id = Role::where('nombre', 'admin')->first()->id;
-
         $user->save();
 
+        $user2 = new User([
+            'name' => 'user',
+            'username' => 'user',
+            'email' => 'user@user.com',
+            'password' => bcrypt('user'),
+        ]);
+        $user2->role_id = Role::where('nombre', 'user')->first()->id;
+        $user2->save();
+
+        $user3 = new User([
+            'name' => 'Otro',
+            'username' => 'otro',
+            'email' => 'otro@user.com',
+            'password' => bcrypt('otro'),
+        ]);
+        $user3->role_id = Role::where('nombre', 'user')->first()->id;
+        $user3->save();
+
+        $user4 = new User([
+            'name' => 'uno',
+            'username' => 'uno',
+            'email' => 'uno@user.com',
+            'password' => bcrypt('uno'),
+        ]);
+        $user4->role_id = Role::where('nombre', 'user')->first()->id;
+        $user4->save();
     }
 }
