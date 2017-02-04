92d7354fa4849c53d7285e750c96911b9d6aa15f
valentin plechuc <vplechuc@gmail.com>
2017-02-04 01:14:05 -0300

    refactoring invoice controller

--- app/Http/Controllers/Invoice/InvoiceController.php
+++ app/Http/Controllers/Invoice/InvoiceController.php
@@ -2,79 +2,32 @@
 
 namespace App\Http\Controllers\Invoice;
 
-use App\Entities\{Article, Invoice, User};
-use Illuminate\Http\Request;
+use App\Entities\{
+    Article, Invoice, Payment, User
+};
+
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
     public function index(Request $request)
     {
-
         //search options
         $invoices = Invoice::name($request->get('numero'))->orderby('id', 'DESC')->paginate();
-
-
         return view('invoice.index', compact('invoices'));
     }
 
-    public function articles(Request $request)
+    public function create(Request $request)
     {
-        $term = Request::get('term');
+        //search options
+        $payments = Payment::all();
 
-        return Article::findByNameorDescription($term);
-
+        return view('invoice.create', compact('payments'));
     }
 
-    public function user(Request $request)
+    public function store(Request $request)
     {
-        $term = Request::get('term');
-
-        return User::findByNameorUserName($term);
     }
-
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('invoice.create');
-    }
-
-    public function store()
-    {
-        //save invoice info
-
-        //saves invoice_detail info
-
-        //return index
-
-    }
-
-
-    public function show()
-    {
-        //showinvoice info
-    }
-
-
-
-
-    public function storepdf()
-    {
-        //save invoice info
-
-        //saves invoice_detail info
-
-
-        //to pdf
-    }
-
-
-
-
 
 }
