f1548b692fd35bb69b788c041bb47c166ba3ad9b
valentin plechuc <vplechuc@gmail.com>
2017-02-05 01:29:15 -0300

    refactor usersfields .. remove password for rdition

--- resources/views/partials/userFields.blade.php
+++ resources/views/partials/userFields.blade.php
@@ -5,5 +5,5 @@
     {!! Form::label('role_id', 'Rol') !!}
     {!! Form::select('role_id', $roles, $user->role->id, ['class' => 'form-control', 'placeholder' => 'Seleccion una opcion', 'required'])!!}
 </div>
-{!! Field::text('password', null ,['class' => 'form-control']) !!}
+
 
