c6995bee96d953826b6959b6dc40de93cee1a6e7
valentin plechuc <vplechuc@gmail.com>
2017-02-06 01:07:37 -0300

    refactoring create invoice blade template

--- resources/views/invoice/create.blade.php
+++ resources/views/invoice/create.blade.php
@@ -9,135 +9,11 @@
  <div class="panel-heading">Facturacion</div>
   <div class="panel-body">
 
-
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-3">
-                    <label for="fecha">Fecha</label>
-                    <input class="form-control" type="text" placeholder="Fecha" readonly name="fecha"  value="{{ Carbon\Carbon::today()->format('d-m-Y ' )}}"/>
-                </div>
-                <div class="col-md-3">
-
-                </div>
-                <div class="col-md-3">
-                </div>
-                <div class="col-md-3">
-                    <label for="numeroF">Numero Factura</label>
-                    <input class="form-control" type="text" placeholder="Numero" readonly name="numeroF" />
-                </div>
-            </div>
-
-            <hr />
-            <hr />
-
-
-            <div class="row">
-
-                    <div class="well well-sm">
-                        <div class="row">
-                            <div class="col-xs-4">
-                                <input id="client" class="form-control typeahead" type="text" placeholder="client" />
-                            </div>
-
-                            <div class="col-xs-6">
-                                <input class="form-control" type="text" placeholder="Dirección" readonly id="direccion" />
-                            </div>
-                            <div class="col-xs-2">
-                                <input class="form-control" type="text" placeholder="Email" readonly id="email" />
-                            </div>
-                        </div>
-                    </div>
-
-                <div class="row">
-                    <div class="col-xs-7">
-                        <input id="article" class="form-control" type="text" placeholder="Nombre del producto" />
-                    </div>
-                    <div class="col-xs-2">
-                        <input id="cantidad" class="form-control" type="text" placeholder="Cantidad" />
-                    </div>
-                    <div class="col-xs-2">
-                        <div class="input-group">
-                            <input class="form-control" type="text" placeholder="Precio" id="precio" readonly />
-                        </div>
-                    </div>
-                    <div class="col-xs-1">
-                        <button onclick="agregarProducto()" class="btn btn-primary form-control" id="btn-agregar">
-                            <i class="glyphicon glyphicon-plus"></i>
-                        </button>
-                    </div>
-                </div>
-
-            </div>
-
-            <hr />
-            <hr />
-
-            <div class="row">
-                <div class="col-md-12">
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th style="width:40px;"></th>
-                            <th>Id</th>
-                            <th>Producto</th>
-                            <th style="width:100px;">Cantidad</th>
-                            <th style="width:100px;">P. Unitario</th>
-                            <th style="width:100px;">Total</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr each={detail}>
-                            <td>
-                                <button onclick="deleteDetailRow()" class="btn btn-danger btn-xs">X</button>
-                            </td>
-                            <td>id</td>
-                            <td>@{{ nombre }}</td>
-                            <td class="text-right">@{{ cantidad }}</td>
-                            <td class="text-right">$ @{{ precio }}</td>
-                            <td class="text-right">$ @{{ total }}</td>
-                        </tr>
-                        </tbody>
-                        <tfoot>
-
-                        <tr>
-                            <td colspan="4" class="text-right"><b>Total</b></td>
-                            <td class="text-right">$ @{{ total }}</td>
-                        </tr>
-                        </tfoot>
-                    </table>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="form-group">
-                        {!! Form::label('payment_id', 'Medio de pago') !!}
-                        {!! Form::select('payment_id', $payments, null, ['class' => 'form-control', 'placeholder' => 'Seleccione una opcion', 'required'])!!}
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="btn-group">
-
-                        {!! Field::text('Cupon  ') !!}
-
-                    </div>
-                </div>
-                <div class="col-md-4">
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-4">
-                  <button  id="grabar" class="btn btn-primary btn-lg">Grabar</button>
-                </div>
-                <div class="col-md-4">
-                    <button type="button" class="btn btn-primary btn-lg">Imprimir</button>
-                </div>
-                <div class="col-md-4">
-                    <button type="button" class="btn btn-warning">Cancelar</button>
-                </div>
-            </div>
-        </div>
+      <div class="row">
+          <div class="col-md-12">
+              <invoice></invoice>
+          </div>
+      </div>
 
 
   </div>
@@ -147,6 +23,13 @@
 
 @section('scripts')
     <script src="{{ asset('js/jquery.easy-autocomplete.js') }}"></script>
+    <script src="{{asset('riot_components/invoice.tag')}}" type="riot/tag"></script>
+    <script>
+        $(document).ready(function(){
+            riot.mount('invoice');
+        })
+    </script>
+
 
     <script>
 
