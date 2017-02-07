af955f8adb2e0b8637902e4141327e0d4b278b26
valentin plechuc <vplechuc@gmail.com>
2017-02-06 23:43:55 -0300

    rafactor relatioship on Invoice detail

--- app/Entities/Invoice_Detail.php
+++ app/Entities/Invoice_Detail.php
@@ -6,14 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invoice_Detail extends Model
 {
+    //protected $table = 'invoice_details';
     protected $fillable = ['invoice_id', 'cantidad', 'article_id', 'precio', 'total_line'];
 
 
-    public function invoice(){
-        return $this->belongsTo(Invoice::class);
-    }
+//    public function invoice(){
+//        return $this->belongsTo(Invoice::class);
+//    }
 
     public function articles(){
-        return $this->hasMany(Article::class);
+        return $this->belongsTo(Article::class);
     }
 }
