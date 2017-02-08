7b7abe360340b89622f6d5277899439a903304d6
valentin plechuc <vplechuc@gmail.com>
2017-02-08 19:02:43 -0300

    adding method showuser... needs to be refactored

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers\Report;
 
 
 use App\Entities\Client;
+use App\Entities\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ReportController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
 
-        return view('report.sales.byclient.index');
+        $report_name = 'report.sales.'. $request->filter . '.index';
+
+
+        return view($report_name);
     }
 
     public function show(Request $request)
@@ -30,4 +34,17 @@ class ReportController extends Controller
         return view('report.sales.byclient.show' , compact('invoices', 'cliente'));
     }
 
+    public function showuser(Request $request)
+    {
+        $user_id = $request->input('user_id');
+
+        $invoices = \DB::table('invoices')
+            ->select('user_id','id','created_at','total')
+            ->where('client_id', "$user_id")
+            ->get();
+
+        $user = User::find($user_id);
+
+        return view('report.sales.byuser.show' , compact('invoices', 'user'));
+    }
 }
