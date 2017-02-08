5be6c83a02f604329282feee7f1bb60345462df9
valentin plechuc <vplechuc@gmail.com>
2017-02-08 20:34:18 -0300

    views changes

---
+++ resources/views/report/sales/daily/show.blade.php
@@ -0,0 +1,38 @@
+@extends('app')
+
+@section('content')
+
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Reporte por fechas</div>
+                <div class="panel-body">
+
+                <h1>No implementado</h1>
+
+
+                    {{--<table class="table table-striped">--}}
+                        {{--<thead>--}}
+                        {{--<th>Fecha</th>--}}
+                        {{--<th>Comprobante</th>--}}
+                        {{--<th>Total</th>--}}
+                        {{--</thead>--}}
+                        {{--<tbody>--}}
+                        {{--@foreach($invoices as $invoice)--}}
+                            {{--<tr>--}}
+
+                                {{--<td>{{$invoice->created_at }}</td>--}}
+                                {{--<td> {{ str_pad ($invoice->id, 7, '0', STR_PAD_LEFT) }}</td>--}}
+                                {{--<td>{{$invoice->total }}</td>--}}
+
+                            {{--</tr>--}}
+                        {{--@endforeach--}}
+                        {{--</tbody>--}}
+                    {{--</table>--}}
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
